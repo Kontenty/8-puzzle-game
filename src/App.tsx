4289190bd066cfc7974036fff5f97319d023902c
Konrad Gorski <konrad_gorski@docupace.com>
2025-06-05 10:42:41 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <div className="flex">
+        <h1 className="text 2xl">8-Puzzle</h1>
+        <button>Shuffle</button>
+        <button>Moves</button>
+      </div>
+      <div className="width-[511px"></div>
+    </main>
+  );
+}
+
+export default App;
