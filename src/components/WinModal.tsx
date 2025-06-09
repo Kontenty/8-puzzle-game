6273b0d37018f7a4eec2b1be676e166beec98c8e
Konrad Gorski <konradgrsk@gmail.com>
2025-06-09 15:45:44 +0200

    feat: add modal animation, easly close modal"

--- src/components/WinModal.tsx
+++ src/components/WinModal.tsx
@@ -1,10 +1,26 @@
+import { motion } from "framer-motion";
 type Props = {
   onClick: () => void;
 };
 const WinModal = ({ onClick }: Props) => {
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto bg-gray-200/70 flex justify-center items-center">
-      <div className="bg-white/75 py-8 px-12 rounded-2xl">
+    <div
+      className="fixed z-10 inset-0 overflow-y-auto bg-gray-200/70 flex justify-center items-center"
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          onClick();
+        }
+      }}
+    >
+      <motion.div
+        initial={{ opacity: 0, scale: 0.7 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.7 }}
+        className="bg-white/75 py-10 px-14 rounded-2xl"
+      >
         <h2 className="text-2xl text-center mb-2">Congratulations !</h2>
         <button
           className="bg-rose-100 py-3 px-8 rounded-xl shadow-lg hover:bg-rose-200 transition"
@@ -12,7 +28,7 @@ const WinModal = ({ onClick }: Props) => {
         >
           <span className="text-5xl text-rose-800 text-center">You won</span>
         </button>
-      </div>
+      </motion.div>
     </div>
   );
 };
