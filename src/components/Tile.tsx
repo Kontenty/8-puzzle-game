a950ba812dcfb7ed076636bf19e5a50b161ae228
Konrad Gorski <konradgrsk@gmail.com>
2025-06-09 15:19:20 +0200

    feat: add board animation with framer

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,4 +1,5 @@
 import "./Tile.css";
+import { motion } from "framer-motion";
 
 const Tile = ({
   tileIndex,
@@ -12,17 +13,25 @@ const Tile = ({
   handleTileClick: (clickedIndex: number) => void;
 }) => {
   return (
-    <button
+    <motion.button
+      layout
+      transition={{ type: "spring", stiffness: 300, damping: 20 }}
+      whileHover={{
+        scale: 1.025,
+        boxShadow: "0px 0px 8px -1px rgba(0, 0, 0, 0.3)",
+        zIndex: 1, // Bring to front slightly (useful if tiles are close)
+      }}
       onClick={() => handleTileClick(index)}
-      className={`tile ${emptyIndex === index ? "empty" : ""}`}
+      className={`tile${emptyIndex === index ? " empty" : ""}`}
       style={{
         backgroundPosition: `${(tileIndex % 3) * -100}% ${
           Math.floor(tileIndex / 3) * -100
         }%`,
       }}
+      type="button"
     >
       {tileIndex}
-    </button>
+    </motion.button>
   );
 };
 
